688f207fd32e6ad8a70bd1b5791f67ac224615ab
RouxRhett <nsch.4871@gmail.com>
2025-05-24 22:32:12 +0900

    [change]rubocop実行

--- app/controllers/admin/users/registrations_controller.rb
+++ app/controllers/admin/users/registrations_controller.rb
@@ -38,12 +38,12 @@ class Admin::Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [ :create ]
 
   protected
 
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:admin])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :admin ])
   end
 
   def build_resource(hash = {})
