f5a7fb35d9439727c75776b4f8538846dfabcd41
RouxRhett <nsch.4871@gmail.com>
2025-05-25 22:40:00 +0900

    [change]書籍一覧画面をログイン状態かつUserのadmin:trueのみアクセス可能に変更

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,14 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!   # ログインチェック
+  before_action :check_admin          # admin: true か
   def index
     @products = Product.all
+  end
+  private
+
+  def check_admin
+    unless current_user&.admin?
+      redirect_to root_path # トップページに戻す
+    end
   end
 end
