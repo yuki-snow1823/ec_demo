6f401184fc577dd12e76b07f01f6815d4093a139
RouxRhett <nsch.4871@gmail.com>
2025-05-25 21:47:31 +0900

    [change]productのseed追記とrubocop実施

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,20 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+  # This file should ensure the existence of records required to run the application in every environment (production,
+  # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+  # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+  #
+  # Example:
+  #
+  #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+  #     MovieGenre.find_or_create_by!(name: genre_name)
+  #   end
+
+  prices = [ 0, 100, 350, 1000, 3500 ]
+  stocks = [ 0, 10, 35, 100, 1000 ]
+  5.times do |n|
+    Product.create!(
+      name: "書籍名#{n + 1}",
+      description: "書籍の説明です。書籍の説明です。書籍の説明です。書籍の説明です。",
+      price: prices[n],
+      stock: stocks[n]
+    )
+  end
