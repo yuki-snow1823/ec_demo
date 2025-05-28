afcfc7afcb116d48dbdf2efd397c0a003f77a2ea
RouxRhett <nsch.4871@gmail.com>
2025-05-28 12:49:46 +0900

    [change]Productのindexに書かれていた条件ロジックをヘルパーに移動

--- app/helpers/admin/products_helper.rb
+++ app/helpers/admin/products_helper.rb
@@ -1,2 +1,13 @@
 module Admin::ProductsHelper
+  def formatted_price(product)
+    "#{product.price} 円"
+  end
+
+  def formatted_stock(product)
+    product.stock.zero? ? "在庫切れ" : "#{product.stock} 個"
+  end
+
+  def publication_status(product)
+    product.active ? "公開" : "非公開"
+  end
 end
