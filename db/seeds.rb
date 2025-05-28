1fc2b42782a1bcd519f0efca03250f76dd1c0840
RouxRhett <nsch.4871@gmail.com>
2025-05-28 12:16:58 +0900

    [change]Product生成seedの記述方法を変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,14 @@
   #     MovieGenre.find_or_create_by!(name: genre_name)
   #   end
 
-  prices = [ 0, 100, 350, 1000, 3500 ]
-  stocks = [ 0, 10, 35, 100, 1000 ]
-  active = [ true, false, true, false, true ]
-  5.times do |n|
-    Product.create!(
-      name: "書籍名#{n + 1}",
-      description: "書籍の説明です。書籍の説明です。書籍の説明です。書籍の説明です。",
-      price: prices[n],
-      stock: stocks[n],
-      active: active[n]
-    )
-  end
+  books = [
+  { name: "書籍名1", description: "書籍の説明です。", price: 0,    stock: 0,   active: true  },
+  { name: "書籍名2", description: "書籍の説明です。", price: 100,  stock: 10,  active: false },
+  { name: "書籍名3", description: "書籍の説明です。", price: 350,  stock: 35,  active: true  },
+  { name: "書籍名4", description: "書籍の説明です。", price: 1000, stock: 100, active: false },
+  { name: "書籍名5", description: "書籍の説明です。", price: 3500, stock: 1000, active: true  }
+]
+
+books.each do |attrs|
+  Product.create!(attrs)
+end
