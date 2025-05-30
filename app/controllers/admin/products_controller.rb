411adc77605b4cb615d87cf66b676d60c82b057e
koguchi-e <koguchi.e915@gmail.com>
2025-05-30 09:50:22 +0900

    products_controller.rbスペース・インテンド修正

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,18 +1,19 @@
 class Admin::ProductsController < ApplicationController
-    def new
-    end
-    def create
-        @product = Product.new(product_params)
-        if @product.save
-            redirect_to root_path
-        else
-            render :new
-        end
-    end
+  def new
+  end
 
-    private
-
-    def product_params
-        params.require(:product).permit(:name, :author, :price, :stock, :description, :active)
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
     end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :author, :price, :stock, :description, :active)
+  end
 end
