c40c924ecf7399ac5d81e605e9751a2825e8851b
Yuki Horikoshi <59280290+yuki-snow1823@users.noreply.github.com>
2025-06-01 22:40:56 +0900

    Revert "書籍新規登録アクションを作成"

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,6 @@ Rails.application.routes.draw do
     registrations: "users/registrations"
   }, as: "public_users"
 
-  namespace :admin do
-    resources :products, only: [ :new, :create ]
-  end
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
