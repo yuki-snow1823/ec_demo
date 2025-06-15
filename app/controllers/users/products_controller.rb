99ecc79613eced3144485175cd96f6f24fe07cbb
koguchi.e <koguchi.e915@gmail.com>
2025-06-15 10:13:10 +0900

    users/productsコントローラーの作成

--- app/controllers/users/products_controller.rb
+++ app/controllers/users/products_controller.rb
@@ -1,9 +1,12 @@
 class Users::ProductsController < ApplicationController
   def index
+<<<<<<< HEAD
     @products = Product.where(active: true)
   end
 
   def show
     @product = Product.find(params[:id])
+=======
+>>>>>>> 4fe7621 (users/productsコントローラーの作成)
   end
 end
