44e262837baf3700b90542a3384cdb08bc6a1eb8
koguchi-e <koguchi.e915@gmail.com>
2025-06-24 11:37:17 +0900

    admin・user共通のproductsヘルパーにstock_labelメソッドを追加

--- app/helpers/admin/products_helper.rb
+++ app/helpers/admin/products_helper.rb
@@ -1,8 +1,8 @@
 module Admin::ProductsHelper
-  def stock_label(product)
-    return "在庫切れ" if product.stock.to_i <= 0
-    "#{product.stock} 個"
-  end
+  # def stock_label(product)
+  #   return "在庫切れ" if product.stock.to_i <= 0
+  #   "#{product.stock} 個"
+  # end
 
   def publication_status(product)
     product.active ? "公開" : "非公開"
